9cb405f78e985bd80fa52b4782c54b5287309c5f
dnaroma <me@dna.moe>
2026-04-11 10:54:47 +0200

    refactor(content-site): remove region switcher state

--- apps/content-site/src/routes/+layout.server.ts
+++ apps/content-site/src/routes/+layout.server.ts
@@ -1,20 +1,8 @@
 import type { LayoutServerLoad } from "./$types";
-import {
-  DEFAULT_SECONDARY_REGION,
-  normalizeRegion,
-  normalizeUiLocale,
-  PRIMARY_REGION_COOKIE_NAME,
-  SECONDARY_REGION_COOKIE_NAME,
-  UI_LOCALE_COOKIE_NAME
-} from "$lib/region";
+import { normalizeUiLocale, UI_LOCALE_COOKIE_NAME } from "$lib/region";
 
 export const load: LayoutServerLoad = ({ cookies }) => {
   return {
-    primaryRegion: normalizeRegion(cookies.get(PRIMARY_REGION_COOKIE_NAME)),
-    secondaryRegion: normalizeRegion(
-      cookies.get(SECONDARY_REGION_COOKIE_NAME),
-      DEFAULT_SECONDARY_REGION
-    ),
     uiLocale: normalizeUiLocale(cookies.get(UI_LOCALE_COOKIE_NAME))
   };
 };
